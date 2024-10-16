f02006fe53f7cab30cde20a780ba825cbbc7b2f2
yuki0416f <ydk60ynee85p@gmail.com>
2024-10-16 13:53:32 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP007課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        $Array = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+         print_r($Array);
+        ?>
+    </p>
+</body>
+</html>
